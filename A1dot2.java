68b6e8e0d4982f8dfa18d569d26ab7266e641b68
Martha Haag <43384571+mintner@users.noreply.github.com>
2019-02-14 15:15:49 -0500

    Lesson01
    Chapter 1 Practice Exercises

---
+++ A1dot2.java
@@ -0,0 +1,15 @@
+
+
+public class A1dot2 {
+
+	public static void main(String[] args) {
+		//Display message Welcome to Java on the console 5 times
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+
+	}
+
+}
